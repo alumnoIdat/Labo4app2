bd1d345346f6c241450541c258faf4d2b38bc4ac
alumnoIdat <leomanarael@gmail.com>
2016-09-05 10:46:39 -0500

    Terminado

--- app/src/main/java/com/example/chango/labo4app2/MainActivity.java
+++ app/src/main/java/com/example/chango/labo4app2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.chango.labo4app2;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -10,5 +11,29 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("HelloWorld", "onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("HelloWorld", "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("HelloWorld", "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("HelloWorld", "onResume");
+        super.onResume();
     }
 }
